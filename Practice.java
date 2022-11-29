e4a9862b711d8e77c7c08d729f3674da5be4b615
Harshavardhan Reddy <harsha.reddy6196@gmail.com>
2022-11-29 17:42:20 +0000

    Changed name Practice.txt to practice.java

---
+++ Practice.java
@@ -0,0 +1,9 @@
+//This code is the sample one
+class Simple{
+  public static void main(String args[]){
+    System.out.println("Hello Java");
+     // print "welcome to Git" in the terminal window: 
+    System.out.println("Welcome To gGit");
+
+  }
+}
