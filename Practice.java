ff0bf994b269b41a6bd01f490e0b124ed46fc695
Harshavardhan Reddy <harsha.reddy6196@gmail.com>
2022-12-01 11:01:07 +0000

    Added new line in the privious code

--- Practice.java
+++ Practice.java
@@ -3,7 +3,7 @@ class Simple{
   public static void main(String args[]){
     System.out.println("Hello Java");
      // print "welcome to Git" in the terminal window: 
-    System.out.println("Welcome To gGit");
-
+     System.out.println("Welcome To gGit");
+     System.out.println("Welcome to New begin");
   }
 }
